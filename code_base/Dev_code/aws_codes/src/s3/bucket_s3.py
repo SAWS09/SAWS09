5f50f1f8ab4bfd85081666ae5a506c91406b9b19
Sourabhs Repo <95543790+SAWS09@users.noreply.github.com>
2022-01-08 00:29:49 +0530

    Feature/nwh boto with s3 (#5)
    * Added code base folder and files
    
    * Updated folder struct and req file
    
    * Added new folder structure

---
+++ code_base/Dev_code/aws_codes/src/s3/bucket_s3.py
@@ -0,0 +1,25 @@
+import boto3
+
+# all_regions = ['us-east-1', 'us-west-1', 'us-west-2', 'us-east2', 'eu-west-1', 'eu-west-2', 'eu-central-1',
+#                'eu-north-1', 'eu-south-1', 'ap-southeast-1', 'ap-southeast-2', 'ap-south-1', 'sa-east-1']
+
+client = boto3.client('ec2')
+all_regions= regions = client.describe_regions()
+
+for region in all_regions['Regions'][0]['RegionName']:
+    # def_vpc = client.create_default_vpc()
+    # print(def_vpc)
+
+    desc_resp = client.describe_vpcs(
+        Filters=[
+            {
+                'Name': 'isDefault',
+                'Values': [
+                    'True',
+                ]
+            },
+        ]
+    )
+    print(desc_resp)
+
+
